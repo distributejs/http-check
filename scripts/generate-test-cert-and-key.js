a45a0d7d7704f1e175ee1fb49c7db34ea9e33291
mmaciusowicz <mmaciusowicz@users.noreply.github.com>
2019-07-23 22:01:17 +0000

    Update path to tests directory in keygen script; Remove call to keygen script from install script

--- scripts/generate-test-cert-and-key.js
+++ scripts/generate-test-cert-and-key.js
@@ -8,6 +8,6 @@ const selfsigned = require("selfsigned");
 
 var pem = selfsigned.generate([{ name: 'commonName', value: 'localhost' }], { days: 365 });
 
-fs.writeFileSync(path.join(__dirname, "../test/fixtures/ssh/cert.pem"), pem.cert);
+fs.writeFileSync(path.join(__dirname, "../tests/fixtures/ssh/cert.pem"), pem.cert);
 
-fs.writeFileSync(path.join(__dirname, "../test/fixtures/ssh/key.pem"), pem.private);
+fs.writeFileSync(path.join(__dirname, "../tests/fixtures/ssh/key.pem"), pem.private);
